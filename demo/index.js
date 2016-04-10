b46e7e2bb22658aa45d023c915251a5d795efb12
Conor Hastings <hastings.conorm@gmail.com>
2016-04-10 11:42:26 -0400

    update demo

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import SyntaxHighlighter from '../dist';
-import 'highlight.js/styles/docco.css';
+
 const availableStyles = [
   'agate',
   'androidstudio',
@@ -29,7 +29,6 @@ const availableStyles = [
   'atelier-sulphurpool-dark',
   'atelier-sulphurpool-light',
   'brown-paper',
-  'brown-papersq.png',
   'codepen-embed',
   'color-brewer',
   'dark',
@@ -56,11 +55,9 @@ const availableStyles = [
   'paraiso-dark',
   'paraiso-light',
   'pojoaque',
-  'pojoaque.jpg',
   'railscasts',
   'rainbow',
   'school-book',
-  'school-book.png',
   'solarized-dark',
   'solarized-light',
   'sunburst',
@@ -74,50 +71,66 @@ const availableStyles = [
   'zenburn' 
 ];
 class Component extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			selected: 'docco'
-		}
-	}
-	render() {
-		const h1Style = {
-		  fontSize: 42,
-		  color: 'aliceblue'
-		};
-		const h2 = {
-		  fontSize: 24,
-		  color: 'aliceblue'
-		}
-		const dropDown = (
-			<select value={this.state.selected} onChange={(e) => this.setState({selected: e.target.value})}>
-				{availableStyles.map(s => <option key={s} value={s}>{s}</option>)}
-			</select>
-		);
-		return (
-			<div>
-				<h1 style={h1Style}>React Syntax Highlighter</h1>
-				<h2 style={h2}>Change Style</h2>
-				{dropDown}
-				<div>
-					<SyntaxHighlighter language='javascript' stylesheet={this.state.selected}>
-						{`
-							const woah = fun => fun + 1;
-							const dude = woah(2) + 3;
-							function thisIsAFunction() {
-								return [1,2,3].map(n => n + 1).filter(n !== 3);
-							}
-							console.log('making up fake code is really hard');
+  constructor() {
+    super();
+    const initialCodeString = `const woah = fun => fun + 1;
+const dude = woah(2) + 3;
+function thisIsAFunction() {
+  return [1,2,3].map(n => n + 1).filter(n !== 3);
+}
+console.log('making up fake code is really hard');
 
-							function itIs() {
-								return 'no seriously really it is';
-							}
-						`}
-					</SyntaxHighlighter>
-				</div>
-			</div>
-		);
-	}
+function itIs() {
+  return 'no seriously really it is';
+}
+  `;
+    this.state = {
+      selected: 'docco',
+      style: require('../dist/styles/docco').default,
+      code: initialCodeString
+    }
+  }
+  render() {
+    const h1Style = {
+      fontSize: 42,
+      color: 'aliceblue'
+    };
+    const h2 = {
+      fontSize: 24,
+      color: 'aliceblue'
+    }
+
+    const dropDown = (
+      <select 
+        value={this.state.selected} 
+        onChange={(e) => this.setState({style: require(`../dist/styles/${e.target.value}`).default, selected: e.target.value})}
+      >
+        {availableStyles.map(s => <option key={s} value={s}>{s}</option>)}
+      </select>
+    );
+
+    return (
+      <div>
+        <h1 style={h1Style}>React Syntax Highlighter</h1>
+        <h2 style={h2}>Change Style</h2>
+        {dropDown}
+        <div style={{paddingTop: 20}}>
+          <textarea 
+            style={{float: 'left', marginTop: 11}} 
+            rows={40} 
+            cols={100} 
+            value={this.state.code}
+            onChange={(e) => this.setState({code: e.target.value})}
+          />
+          <div style={{float: 'right', width: '50%'}}>
+            <SyntaxHighlighter language='javascript' style={this.state.style}>
+              {this.state.code}
+            </SyntaxHighlighter>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 
