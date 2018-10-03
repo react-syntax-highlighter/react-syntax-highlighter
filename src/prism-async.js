47aba6f3e65f866557dda675f6d9c898f792240f
Marco De Jongh <mdejongh@atlassian.com>
2018-10-03 16:41:26 +1000

    Clean up promises

--- src/prism-async.js
+++ src/prism-async.js
@@ -2,26 +2,36 @@ import React from 'react';
 import highlight from './highlight';
 
 const languages = [];
-export const registerLanguage = (_, language) => languages.push(language);
+export const registerLanguage = (_, language) => {
+  if(ReactHighlightAsync.refractor) {
+    return refractor.register(language);
+  } else {
+    languages.push(language);
+  }
+};
 
 export default class ReactHighlightAsync extends React.PureComponent {
   static refractor = null;
-  static highlightInstance = null;
-
-  state = {
-    refractor: ReactHighlightAsync.refractor
-  };
+  static highlightInstance = (highlight(null, {}));
+  static refractorPromise = null;
+  
+  _loadRefractor() {
+    ReactHighlightAsync.refractorPromise = import(/* webpackChunkName:"react-syntax-highlighter/refractor-import" */ 'refractor/core').then(({ default: refractor }) => {
+      languages.forEach((language) => refractor.register(language));
+      ReactHighlightAsync.refractor = refractor;
+    });
+  }
 
   componentWillMount() {
-    if (!ReactHighlightAsync.highlightInstance) {
-      const refractorPromise = import(/* webpackChunkName:"react-syntax-highlighter/refractor-import" */ 'refractor/core').then(({ default: refractor }) => {
-        languages.forEach((language) => refractor.register(language));
-        this.setState({ refractor })
-      });
-      ReactHighlightAsync.highlightInstance = (highlight(null, {}));
+    if (!ReactHighlightAsync.refractorPromise) {
+      this._loadRefractor();
     }
+
+    ReactHighlightAsync.refractorPromise.then(() => {
+      this.forceUpdate();
+    });
   }
   render() {
-    return (<ReactHighlightAsync.highlightInstance {...this.props} astGenerator={this.state.refractor} />);
+    return (<ReactHighlightAsync.highlightInstance {...this.props} astGenerator={ReactHighlightAsync.refractor} />);
   }
 };
