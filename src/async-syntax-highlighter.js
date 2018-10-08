9af398349bde269d57da906f97e09248460f7253
Marco De Jongh <mdejongh@atlassian.com>
2018-10-08 14:54:07 +1100

    Add async loaders for the other highlighters
    Breaking change because it changes the api of prism-async.
    Fixes #136

---
+++ src/async-syntax-highlighter.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import highlight from './highlight';
+
+export default (options) => {
+  const supportsLanguageRegistering = !!options.supportsLanguageRegistering
+  const loader = options.loader;
+
+  class ReactAsyncHighlighter extends React.PureComponent {
+    static astGenerator = null;
+    static highlightInstance = (highlight(null, {}));
+    static astGeneratorPromise = null;
+    static languages = [];
+
+    static preload() {
+      ReactAsyncHighlighter.loadAstGenerator();
+    }
+
+    static registerLanguage = (_, language) => {
+      if (!supportsLanguageRegistering) {
+        return;
+      }
+      
+      if(ReactAsyncHighlighter.astGenerator) {
+        return ReactAsyncHighlighter.astGenerator.register(language);
+      } else {
+        ReactAsyncHighlighter.languages.push(language);
+      }
+    };
+    
+
+    static loadAstGenerator() {
+      ReactAsyncHighlighter.astGeneratorPromise = loader().then(astGenerator => {
+        ReactAsyncHighlighter.astGenerator = astGenerator;
+
+        if (supportsLanguageRegistering && ReactAsyncHighlighter.languages.length) {
+          ReactAsyncHighlighter.languages.forEach((language) => astGenerator.register(language));
+        }
+      });
+      
+      return ReactAsyncHighlighter.astGeneratorPromise;
+    }
+  
+    componentDidMount() {
+      if (!ReactAsyncHighlighter.astGeneratorPromise) {
+        ReactAsyncHighlighter.loadAstGenerator();
+      }
+  
+      if(!ReactAsyncHighlighter.astGenerator) {
+        ReactAsyncHighlighter.astGeneratorPromise.then(() => {
+          this.forceUpdate();
+        });
+      }
+    }
+  
+    render() {
+      return (<ReactAsyncHighlighter.highlightInstance {...this.props} astGenerator={ReactAsyncHighlighter.astGenerator} />);
+    }
+  };
+
+  return ReactAsyncHighlighter;
+}
