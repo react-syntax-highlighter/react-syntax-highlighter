bfd477e6ae76c9f8b57b599e0876ca25d65b6724
Marco De Jongh <mdejongh@atlassian.com>
2018-10-22 14:19:36 +1100

    Rename index -> default-highlight

--- src/default-highlight.js
+++ src/default-highlight.js
@@ -3,4 +3,3 @@ import defaultStyle from './styles/hljs/default-style';
 import lowlight from 'lowlight';
 
 export default highlight(lowlight, defaultStyle);
-export { default as createElement } from './create-element';
