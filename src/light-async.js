69b99824f71fc75d1d7b2092b0efa59e4d9f7461
Marco De Jongh <mdejongh@atlassian.com>
2018-10-22 16:28:47 +1100

    Fix webpack 3 async imports

--- src/light-async.js
+++ src/light-async.js
@@ -4,5 +4,8 @@ export default createAsyncLoadingHighlighter({
   supportsLanguageRegistering: true,
   loader: () => import(
     /* webpackChunkName:"react-syntax-highlighter/lowlight-import" */ 
-    'lowlight/lib/core').then(({ default: lowlight }) => lowlight),
+    'lowlight/lib/core').then((module) => {
+      // Webpack 3 returns module.exports as default as module, but webpack 4 returns module.exports as module.default
+      return module.default || module;
+    }),
 });
--- src/prism-async.js
+++ src/prism-async.js
@@ -3,5 +3,8 @@ import createAsyncLoadingHighlighter from './async-syntax-highlighter';
 export default createAsyncLoadingHighlighter({
   loader: () => import(
     /* webpackChunkName:"react-syntax-highlighter/refractor-import" */ 
-    'refractor').then(({ default: refractor }) => refractor),
+    'refractor').then((module) => {
+      // Webpack 3 returns module.exports as default as module, but webpack 4 returns module.exports as module.default
+      return module.default || module;
+    }),
 });
