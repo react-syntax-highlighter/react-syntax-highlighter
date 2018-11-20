b01981a41abf3c8b9813c01652816c94104222c0
Marco De Jongh <mdejongh@atlassian.com>
2018-11-20 11:34:29 +1100

    Add supported languages array to normal prism and highlight

--- src/default-highlight.js
+++ src/default-highlight.js
@@ -1,5 +1,9 @@
 import highlight from './highlight';
 import defaultStyle from './styles/hljs/default-style';
 import lowlight from 'lowlight';
+import supportedLanguages from './languages/hljs/supported-languages';
 
-export default highlight(lowlight, defaultStyle);
+const highlighter = highlight(lowlight, defaultStyle);
+highlighter.supportedLanguages = supportedLanguages;
+
+export default highlighter;
