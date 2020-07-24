a8df31cd249f9a707d8afad457e17b78d572617e
David Simmer <dsimmer@netflix.com>
2020-07-23 20:51:25 -0700

    rename default link

--- demo/examples-links.js
+++ demo/examples-links.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ExamplesLinks = () => {
   const demos = [
     {
-      label: 'Default',
+      label: 'Highlight.js (default)',
       path: '/demo/'
     },
     {
