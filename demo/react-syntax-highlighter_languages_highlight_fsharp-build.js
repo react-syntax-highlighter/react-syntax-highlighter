1b861ecf8d076a3155f494c8fca236c9e48af918
David Simmer <dsimmer@netflix.com>
2020-07-29 18:11:24 -0700

    nope! can't gitignore that

---
+++ demo/react-syntax-highlighter_languages_highlight_fsharp-build.js
@@ -0,0 +1,86 @@
+(window['webpackJsonp'] = window['webpackJsonp'] || []).push([
+  ['react-syntax-highlighter_languages_highlight_fsharp'],
+  {
+    /***/ './node_modules/highlight.js/lib/languages/fsharp.js':
+      /*!***********************************************************!*\
+  !*** ./node_modules/highlight.js/lib/languages/fsharp.js ***!
+  \***********************************************************/
+      /*! no static exports found */
+      /***/ function(module, exports) {
+        /*
+Language: F#
+Author: Jonas Follesø <jonas@follesoe.no>
+Contributors: Troy Kershaw <hello@troykershaw.com>, Henrik Feldt <henrik@haf.se>
+Website: https://docs.microsoft.com/en-us/dotnet/fsharp/
+Category: functional
+*/
+        function fsharp(hljs) {
+          var TYPEPARAM = {
+            begin: '<',
+            end: '>',
+            contains: [
+              hljs.inherit(hljs.TITLE_MODE, { begin: /'[a-zA-Z0-9_]+/ })
+            ]
+          };
+
+          return {
+            name: 'F#',
+            aliases: ['fs'],
+            keywords:
+              'abstract and as assert base begin class default delegate do done ' +
+              'downcast downto elif else end exception extern false finally for ' +
+              'fun function global if in inherit inline interface internal lazy let ' +
+              'match member module mutable namespace new null of open or ' +
+              'override private public rec return sig static struct then to ' +
+              'true try type upcast use val void when while with yield',
+            illegal: /\/\*/,
+            contains: [
+              {
+                // monad builder keywords (matches before non-bang kws)
+                className: 'keyword',
+                begin: /\b(yield|return|let|do)!/
+              },
+              {
+                className: 'string',
+                begin: '@"',
+                end: '"',
+                contains: [{ begin: '""' }]
+              },
+              {
+                className: 'string',
+                begin: '"""',
+                end: '"""'
+              },
+              hljs.COMMENT('\\(\\*', '\\*\\)'),
+              {
+                className: 'class',
+                beginKeywords: 'type',
+                end: '\\(|=|$',
+                excludeEnd: true,
+                contains: [hljs.UNDERSCORE_TITLE_MODE, TYPEPARAM]
+              },
+              {
+                className: 'meta',
+                begin: '\\[<',
+                end: '>\\]',
+                relevance: 10
+              },
+              {
+                className: 'symbol',
+                begin: "\\B('[A-Za-z])\\b",
+                contains: [hljs.BACKSLASH_ESCAPE]
+              },
+              hljs.C_LINE_COMMENT_MODE,
+              hljs.inherit(hljs.QUOTE_STRING_MODE, { illegal: null }),
+              hljs.C_NUMBER_MODE
+            ]
+          };
+        }
+
+        module.exports = fsharp;
+
+        /***/
+      }
+  }
+]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_fsharp-build.js.map
---
+++ demo/react-syntax-highlighter_languages_highlight_gcode-build.js
@@ -0,0 +1,97 @@
+(window['webpackJsonp'] = window['webpackJsonp'] || []).push([
+  ['react-syntax-highlighter_languages_highlight_gcode'],
+  {
+    /***/ './node_modules/highlight.js/lib/languages/gcode.js':
+      /*!**********************************************************!*\
+  !*** ./node_modules/highlight.js/lib/languages/gcode.js ***!
+  \**********************************************************/
+      /*! no static exports found */
+      /***/ function(module, exports) {
+        /*
+ Language: G-code (ISO 6983)
+ Contributors: Adam Joseph Cook <adam.joseph.cook@gmail.com>
+ Description: G-code syntax highlighter for Fanuc and other common CNC machine tool controls.
+ Website: https://www.sis.se/api/document/preview/911952/
+ */
+
+        function gcode(hljs) {
+          var GCODE_IDENT_RE = '[A-Z_][A-Z0-9_.]*';
+          var GCODE_CLOSE_RE = '\\%';
+          var GCODE_KEYWORDS = {
+            $pattern: GCODE_IDENT_RE,
+            keyword:
+              'IF DO WHILE ENDWHILE CALL ENDIF SUB ENDSUB GOTO REPEAT ENDREPEAT ' +
+              'EQ LT GT NE GE LE OR XOR'
+          };
+          var GCODE_START = {
+            className: 'meta',
+            begin: '([O])([0-9]+)'
+          };
+          var GCODE_CODE = [
+            hljs.C_LINE_COMMENT_MODE,
+            hljs.C_BLOCK_COMMENT_MODE,
+            hljs.COMMENT(/\(/, /\)/),
+            hljs.inherit(hljs.C_NUMBER_MODE, {
+              begin: '([-+]?([0-9]*\\.?[0-9]+\\.?))|' + hljs.C_NUMBER_RE
+            }),
+            hljs.inherit(hljs.APOS_STRING_MODE, { illegal: null }),
+            hljs.inherit(hljs.QUOTE_STRING_MODE, { illegal: null }),
+            {
+              className: 'name',
+              begin: '([G])([0-9]+\\.?[0-9]?)'
+            },
+            {
+              className: 'name',
+              begin: '([M])([0-9]+\\.?[0-9]?)'
+            },
+            {
+              className: 'attr',
+              begin: '(VC|VS|#)',
+              end: '(\\d+)'
+            },
+            {
+              className: 'attr',
+              begin: '(VZOFX|VZOFY|VZOFZ)'
+            },
+            {
+              className: 'built_in',
+              begin:
+                '(ATAN|ABS|ACOS|ASIN|SIN|COS|EXP|FIX|FUP|ROUND|LN|TAN)(\\[)',
+              end: '([-+]?([0-9]*\\.?[0-9]+\\.?))(\\])'
+            },
+            {
+              className: 'symbol',
+              variants: [
+                {
+                  begin: 'N',
+                  end: '\\d+',
+                  illegal: '\\W'
+                }
+              ]
+            }
+          ];
+
+          return {
+            name: 'G-code (ISO 6983)',
+            aliases: ['nc'],
+            // Some implementations (CNC controls) of G-code are interoperable with uppercase and lowercase letters seamlessly.
+            // However, most prefer all uppercase and uppercase is customary.
+            case_insensitive: true,
+            keywords: GCODE_KEYWORDS,
+            contains: [
+              {
+                className: 'meta',
+                begin: GCODE_CLOSE_RE
+              },
+              GCODE_START
+            ].concat(GCODE_CODE)
+          };
+        }
+
+        module.exports = gcode;
+
+        /***/
+      }
+  }
+]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_gcode-build.js.map
