49cf4d04bef2177a41d5e1a0214ae989b2ddbadf
Karl Horky <karl.horky@gmail.com>
2020-08-27 18:33:46 +0200

    Update vsc-dark-plus.js
    Ref https://github.com/PrismJS/prism-themes/pull/103

--- src/styles/prism/vsc-dark-plus.js
+++ src/styles/prism/vsc-dark-plus.js
@@ -3,7 +3,7 @@ export default {
         "color": "#d4d4d4",
         "fontSize": "13px",
         "textShadow": "none",
-        "fontFamily": "Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+        "fontFamily": "Menlo, Monaco, Consolas, \"Andale Mono\", \"Ubuntu Mono\", \"Courier New\", monospace",
         "direction": "ltr",
         "textAlign": "left",
         "whiteSpace": "pre",
@@ -26,7 +26,7 @@ export default {
         "color": "#d4d4d4",
         "fontSize": "13px",
         "textShadow": "none",
-        "fontFamily": "Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+        "fontFamily": "Menlo, Monaco, Consolas, \"Andale Mono\", \"Ubuntu Mono\", \"Courier New\", monospace",
         "direction": "ltr",
         "textAlign": "left",
         "whiteSpace": "pre",
@@ -43,11 +43,19 @@ export default {
     },
     "pre[class*=\"language-\"]::selection": {
         "textShadow": "none",
-        "background": "#b3d4fc"
+        "background": "#75a7ca"
     },
     "code[class*=\"language-\"]::selection": {
         "textShadow": "none",
-        "background": "#b3d4fc"
+        "background": "#75a7ca"
+    },
+    "pre[class*=\"language-\"] *::selection": {
+        "textShadow": "none",
+        "background": "#75a7ca"
+    },
+    "code[class*=\"language-\"] *::selection": {
+        "textShadow": "none",
+        "background": "#75a7ca"
     },
     ":not(pre) > code[class*=\"language-\"]": {
         "padding": ".1em .3em",
@@ -58,19 +66,25 @@ export default {
     ".namespace": {
         "Opacity": ".7"
     },
+    "doctype .token.doctype-tag": {
+        "color": "#569CD6"
+    },
+    "doctype .token.name": {
+        "color": "#9cdcfe"
+    },
     "comment": {
         "color": "#6a9955"
     },
     "prolog": {
         "color": "#6a9955"
     },
-    "doctype": {
-        "color": "#6a9955"
-    },
-    "cdata": {
-        "color": "#6a9955"
-    },
     "punctuation": {
+        "color": "#d4d4d4"
+    },
+    ".language-html .language-css .token.punctuation": {
+        "color": "#d4d4d4"
+    },
+    ".language-html .language-javascript .token.punctuation": {
         "color": "#d4d4d4"
     },
     "property": {
@@ -86,12 +100,15 @@ export default {
         "color": "#b5cea8"
     },
     "constant": {
-        "color": "#9CDCFE"
+        "color": "#9cdcfe"
     },
     "symbol": {
         "color": "#b5cea8"
     },
     "deleted": {
+        "color": "#b5cea8"
+    },
+    "unit": {
         "color": "#b5cea8"
     },
     "selector": {
@@ -112,87 +129,143 @@ export default {
     "inserted": {
         "color": "#ce9178"
     },
+    ".language-css .token.string.url": {
+        "textDecoration": "underline"
+    },
     "operator": {
-        "color": "#d4d4d4",
-        "background": "#1e1e1e"
+        "color": "#d4d4d4"
     },
     "entity": {
-        "color": "#4ec9b0",
-        "background": "#1e1e1e",
-        "cursor": "unset"
+        "color": "#569cd6"
     },
-    "url": {
-        "color": "#d4d4d4",
-        "background": "#1e1e1e"
-    },
-    ".language-css .token.string": {
-        "color": "#d4d4d4",
-        "background": "#1e1e1e"
-    },
-    ".style .token.string": {
-        "color": "#d4d4d4",
-        "background": "#1e1e1e"
+    "operator.arrow": {
+        "color": "#569CD6"
     },
     "atrule": {
-        "color": "#c586c0"
-    },
-    "attr-value": {
         "color": "#ce9178"
     },
-    "keyword": {
+    "atrule .token.rule": {
         "color": "#c586c0"
     },
+    "atrule .token.url": {
+        "color": "#9cdcfe"
+    },
+    "atrule .token.url .token.function": {
+        "color": "#dcdcaa"
+    },
+    "atrule .token.url .token.punctuation": {
+        "color": "#d4d4d4"
+    },
+    "keyword": {
+        "color": "#569CD6"
+    },
+    "keyword.module": {
+        "color": "#c586c0"
+    },
+    "keyword.control-flow": {
+        "color": "#c586c0"
+    },
+    "keyword.module + .token.maybe-class-name": {
+        "color": "#9cdcfe"
+    },
+    "keyword.module + .token.punctuation + .token.maybe-class-name": {
+        "color": "#9cdcfe"
+    },
     "function": {
+        "color": "#dcdcaa"
+    },
+    "function .token.maybe-class-name": {
         "color": "#dcdcaa"
     },
     "regex": {
         "color": "#d16969"
     },
     "important": {
-        "color": "#d16969",
-        "fontWeight": "bold"
-    },
-    "variable": {
-        "color": "#d16969"
-    },
-    "bold": {
-        "fontWeight": "bold"
+        "color": "#569cd6"
     },
     "italic": {
         "fontStyle": "italic"
     },
     "class-name": {
-        "color": "#4EC9B0"
+        "color": "#4ec9b0"
+    },
+    "maybe-class-name": {
+        "color": "#4ec9b0"
+    },
+    "console": {
+        "color": "#9cdcfe"
     },
     "parameter": {
-        "color": "#9CDCFE"
+        "color": "#9cdcfe"
     },
     "interpolation": {
-        "color": "#9CDCFE"
+        "color": "#9cdcfe"
     },
     "punctuation.interpolation-punctuation": {
         "color": "#569cd6"
+    },
+    "variable": {
+        "color": "#9cdcfe"
+    },
+    "escape": {
+        "color": "#d7ba7d"
+    },
+    "tag .token.punctuation": {
+        "color": "#808080"
+    },
+    "cdata": {
+        "color": "#808080"
+    },
+    "attr-value": {
+        "color": "#ce9178"
+    },
+    "attr-value .token.punctuation": {
+        "color": "#ce9178"
+    },
+    "attr-value .token.punctuation.attr-equals": {
+        "color": "#d4d4d4"
     },
     "namespace": {
         "color": "#4ec9b0"
     },
     "pre[class*=\"language-javascript\"]": {
-        "color": "#4ec9b0"
+        "color": "#9cdcfe"
     },
     "code[class*=\"language-javascript\"]": {
-        "color": "#4ec9b0"
+        "color": "#9cdcfe"
+    },
+    "pre[class*=\"language-jsx\"]": {
+        "color": "#9cdcfe"
+    },
+    "code[class*=\"language-jsx\"]": {
+        "color": "#9cdcfe"
+    },
+    "pre[class*=\"language-typescript\"]": {
+        "color": "#9cdcfe"
+    },
+    "code[class*=\"language-typescript\"]": {
+        "color": "#9cdcfe"
+    },
+    "pre[class*=\"language-tsx\"]": {
+        "color": "#9cdcfe"
+    },
+    "code[class*=\"language-tsx\"]": {
+        "color": "#9cdcfe"
     },
     "pre[class*=\"language-css\"]": {
-        "color": "#CE9178"
+        "color": "#ce9178"
     },
     "code[class*=\"language-css\"]": {
-        "color": "#CE9178"
+        "color": "#ce9178"
     },
     "pre[class*=\"language-html\"]": {
         "color": "#d4d4d4"
     },
     "code[class*=\"language-html\"]": {
         "color": "#d4d4d4"
+    },
+    ".language-regex .token.anchor": {
+        "color": "#dcdcaa"
     },
     ".language-html .token.punctuation": {
         "color": "#808080"
