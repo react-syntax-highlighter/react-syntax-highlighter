788b7d99af37de5d3f90a90ed39d68414bd94a21
Karl Horky <karl.horky@gmail.com>
2020-08-29 22:58:24 +0200

    Style all subsets and permutations of classes

--- src/create-element.js
+++ src/create-element.js
@@ -1,35 +1,61 @@
 import React from 'react';
 
-const classSelectorClassNames = {};
-function classSelectorToClassNames(selector) {
-  if (!selector.includes(".")) return null;
-  if (!classSelectorClassNames[selector]) {
-    classSelectorClassNames[selector] = selector.split(".").filter(Boolean);
-  }
-  return classSelectorClassNames[selector];
+function recursivePermute(arr, permutation = []) {
+  return arr.length === 0
+    ? [permutation]
+    : arr.flatMap((item, i, arr) =>
+        recursivePermute(
+          arr.filter((_, j) => j !== i),
+          [...permutation, item]
+        )
+      );
 }
 
-/** Returns a boolean of whether array1 is a subset of array2 */
-function isSubset(array1, array2) {
-  if (!array1 || !array2) return false;
-  if (!Array.isArray(array1) || !Array.isArray(array2)) return false;
-  const intersection = array1.filter((element) => array2.includes(element));
-  return intersection.length === array1.length;
+const permutations = {};
+function permute(arr) {
+  const key = arr.join('.');
+  if (!permutations[key]) {
+    permutations[key] = recursivePermute(arr);
+  }
+  return permutations[key];
+}
+
+function getSubArrays(arr) {
+  if (arr.length === 1) return [arr];
+  else {
+    const subarr = getSubArrays(arr.slice(1));
+    return subarr.concat(
+      subarr.map(el => el.concat(arr[0])),
+      [[arr[0]]]
+    );
+  }
+}
+
+const classNameCombinations = {};
+function getClassNameCombinations(classNames) {
+  if (classNames.length === 0) return [];
+  if (classNames.length === 1) return classNames;
+  const key = classNames.join('.');
+  if (!classNameCombinations[key]) {
+    classNameCombinations[key] = getSubArrays(classNames)
+      .flatMap(classNames =>
+        classNames.length > 1 ? permute(classNames) : [classNames]
+      )
+      // Less specific (fewer class names) to more specific (more class names)
+      .sort((a, b) => (a.length < b.length ? -1 : 1))
+      .map(arr => arr.join('.'));
+  }
+  return classNameCombinations[key];
 }
 
 export function createStyleObject(classNames, elementStyle = {}, stylesheet) {
-  const newStyles = Object.entries(stylesheet).reduce(
-    (newStyleObject, [selector, styles]) => {
-      if (classNames.includes(selector)) {
-        return { ...styles, ...newStyleObject };
-      } else if (isSubset(classSelectorToClassNames(selector), classNames)) {
-        return { ...newStyleObject, ...styles };
-      }
-      return newStyleObject;
-    },
-    {}
+  const nonTokenClassNames = classNames.filter(
+    className => className !== 'token'
   );
-  return { ...elementStyle, ...newStyles };
+  const classNamesCombinations = getClassNameCombinations(nonTokenClassNames);
+  return classNamesCombinations.reduce((styleObject, className) => {
+    return { ...styleObject, ...stylesheet[className] };
+  }, elementStyle);
 }
 
 export function createClassNameString(classNames) {
@@ -63,28 +89,39 @@ export default function createElement({
     return value;
   } else if (TagName) {
     const childrenCreator = createChildren(stylesheet, useInlineStyles);
-    const nonStylesheetClassNames =
-      useInlineStyles &&
-      properties.className &&
-      properties.className.filter(className => !stylesheet[className]);
-    const className =
-      nonStylesheetClassNames && nonStylesheetClassNames.length
-        ? nonStylesheetClassNames
-        : undefined;
-    const props = useInlineStyles
-      ? {
-          ...properties,
-          ...{ className: className && createClassNameString(className) },
-          style: createStyleObject(
-            properties.className,
-            Object.assign({}, properties.style, style),
-            stylesheet
-          )
-        }
-      : {
-          ...properties,
-          className: createClassNameString(properties.className)
-        };
+
+    let props;
+
+    if (useInlineStyles) {
+      const allStylesheetSelectors = [
+        ...new Set(
+          Object.keys(stylesheet)
+            .map(selector => selector.split('.'))
+            .flat()
+        )
+      ];
+      const className =
+        properties.className &&
+        properties.className.filter(className =>
+          allStylesheetSelectors.includes(className)
+        );
+
+      props = {
+        ...properties,
+        className: createClassNameString(className),
+        style: createStyleObject(
+          properties.className,
+          Object.assign({}, properties.style, style),
+          stylesheet
+        )
+      };
+    } else {
+      props = {
+        ...properties,
+        className: createClassNameString(properties.className)
+      };
+    }
+
     const children = childrenCreator(node.children);
     return (
       <TagName key={key} {...props}>
