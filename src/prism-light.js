bfb43205abf743a55443f814565c823fe5978d97
Cahil Foley <cahilfoley2@gmail.com>
2021-04-09 13:53:30 +0800

    Expose refractor alias function on prism light syntax highlighter
    This allows client applications to register aliases for languages without having to try and access refractor directly. Useful for aliasing languages under other acronyms.

--- src/prism-light.js
+++ src/prism-light.js
@@ -5,4 +5,7 @@ const SyntaxHighlighter = highlight(refractor, {});
 SyntaxHighlighter.registerLanguage = (_, language) =>
   refractor.register(language);
 
+SyntaxHighlighter.alias = (name, aliases) =>
+  refractor.alias(name, aliases);
+
 export default SyntaxHighlighter;
