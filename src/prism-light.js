76d7f1136facee2ccb541e30283bd67ddce4f449
Conor Hastings <hastings.conorm@gmail.com>
2021-11-14 20:13:34 -0800

    Update prism-light.js

--- src/prism-light.js
+++ src/prism-light.js
@@ -5,7 +5,6 @@ const SyntaxHighlighter = highlight(refractor, {});
 SyntaxHighlighter.registerLanguage = (_, language) =>
   refractor.register(language);
 
-SyntaxHighlighter.alias = (name, aliases) =>
-  refractor.alias(name, aliases);
+SyntaxHighlighter.alias = (name, aliases) => refractor.alias(name, aliases);
 
 export default SyntaxHighlighter;
