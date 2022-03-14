bb81ad51d7a567123a4aa3d7d61e0bc9fe4a2111
David Simmer <david@simmer.ooo>
2022-03-13 22:07:38 -0700

    remove empty lines that prettier complains about

--- src/light-async.js
+++ src/light-async.js
@@ -4,9 +4,10 @@ import checkForListedLanguage from './checkForListedLanguage';
 
 export default createAsyncLoadingHighlighter({
   loader: () =>
-    import(/* webpackChunkName:"react-syntax-highlighter/lowlight-import" */
-
-    'lowlight/lib/core').then(module => {
+    import(
+      /* webpackChunkName:"react-syntax-highlighter/lowlight-import" */
+      'lowlight/lib/core'
+    ).then(module => {
       // Webpack 3 returns module.exports as default as module, but webpack 4 returns module.exports as module.default
       return module.default || module;
     }),
--- src/prism-async-light.js
+++ src/prism-async-light.js
@@ -3,9 +3,10 @@ import languageLoaders from './async-languages/prism';
 
 export default createAsyncLoadingHighlighter({
   loader: () =>
-    import(/* webpackChunkName:"react-syntax-highlighter/refractor-core-import" */
-
-    'refractor/core').then(module => {
+    import(
+      /* webpackChunkName:"react-syntax-highlighter/refractor-core-import" */
+      'refractor/core'
+    ).then(module => {
       // Webpack 3 returns module.exports as default as module, but webpack 4 returns module.exports as module.default
       return module.default || module;
     }),
