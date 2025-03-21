e136ecb55216ad897dbe9def5784a9a2f0b05984
Marvin Grvn <marvin.greeven@siemens.com>
2025-03-21 12:34:44 +0100

    fix(deps): change import
    Update prism-async-light.js

--- src/prism-async-light.js
+++ src/prism-async-light.js
@@ -5,7 +5,7 @@ export default createAsyncLoadingHighlighter({
   loader: () =>
     import(
       /* webpackChunkName:"react-syntax-highlighter/refractor-core-import" */
-      'refractor/core'
+      'refractor/lib/core'
     ).then(module => {
       // Webpack 3 returns module.exports as default as module, but webpack 4 returns module.exports as module.default
       return module.default || module;
