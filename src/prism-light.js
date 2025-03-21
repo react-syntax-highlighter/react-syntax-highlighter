f5077fe1540f78984987f58d7e32021db4b65baf
Marvin Grvn <marvin.greeven@siemens.com>
2025-03-21 12:30:01 +0100

    fix(deps): change import
    Update prism-light.js

--- src/prism-light.js
+++ src/prism-light.js
@@ -1,5 +1,5 @@
 import highlight from './highlight';
-import refractor from 'refractor/core';
+import refractor from 'refractor/lib/core.js';
 
 const SyntaxHighlighter = highlight(refractor, {});
 SyntaxHighlighter.registerLanguage = (_, language) =>
