f024a8ca5d3896ee6ee923f11a72d0cd7d7902af
David Simmer <dsimmer@netflix.com>
2025-10-21 23:39:55 -0500

    Revert "chore: bump Webpack to v5 (#594)"
    This reverts commit 7b0027bc0a5be1342b9a8b53c652f56f617d489f.

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: [/node_modules/]
       },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+      { test: /\.css$/, loader: 'style!css' }
     ]
   },
   plugins: [
